65817f2bacee63a9407518ef2cdada34fc226373
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-20 11:22:34 +0700

    mengambil nilai untuk di push dan memanggil fungsi dari push

--- stackUsingLinkedList.cpp
+++ stackUsingLinkedList.cpp
@@ -88,4 +88,12 @@ int main()
         cout << " Enter your choice : ";
         cin >> choice;
         switch (choice)
+        {
+            case 1:
+                cout << "Enter the value to push : ";
+                cin >> value;
+                stact.push(value);
+                break;
+        }
     }
+};
